459d9a1be0b3caca3e4322155099ffdfaa53ebf1
ShamimHossain1 <shamimweb78@gmail.com>
2025-05-04 23:09:46 -0400

    generic interface 2

--- M2-Types/2.4.ts
+++ M2-Types/2.4.ts
@@ -1,26 +1,91 @@
 {
+
+
     // interface - generic
 
-    interface Developer<T>{
+    interface Developer<T,X = null>{
         name: string;
         age: number;
         computer:{
             brand: string;
             model: string;
         }
-        smartWatch:T
+        smartWatch:T,
+        bike?: X
     }
 
 
-    const developer: Developer<string> ={
+    interface SmartWatch{
+        brand: string;
+        model: string;
+    }
+
+    const developer: Developer<SmartWatch> ={
         name: "John Doe",
         age: 30,
         computer:{
             brand: "Apple",
             model: "MacBook Pro"
         },
-        smartWatch:"Apple Watch"
+        smartWatch:{
+            brand: "Apple",
+            model: "Apple Watch"
+        }
     }
+
+
+    type samsungWatch = {
+        brand: string;
+        model: string;
+        releaseYear: number;
+        heartTracker: boolean;
+        waterResistant: boolean;
+    }
+
+    const developer2: Developer<samsungWatch> ={
+        name: "Sauda",
+        age: 30,
+        computer:{
+            brand: "Samsung",
+            model: "Galaxy Book Pro"
+        },
+        smartWatch:{
+            brand: "Samsung",
+            model: "Galaxy Watch",
+            releaseYear: 2022,
+            heartTracker: true,
+            waterResistant: true,
+        }
+    }
+
+
+    const developer3: Developer<samsungWatch, string> ={
+        name: "Sauda",
+        age: 30,
+        computer:{
+            brand: "Samsung",
+            model: "Galaxy Book Pro"
+        },
+        smartWatch:{
+            brand: "Samsung",
+            model: "Galaxy Watch",
+            releaseYear: 2022,
+            heartTracker: true,
+            waterResistant: true,
+        },
+        bike:"BMW"
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
